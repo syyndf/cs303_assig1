b412519f3d9c8f848693c1aff91bc662c04c7fad
Samuel Y <90927762+syyndf@users.noreply.github.com>
2023-05-04 20:03:06 -0500

    Create Functions.h

---
+++ Functions.h
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <string>
+
+using  namespace std;
+
+char userchoice();
+
+void findindex(int arr[], int len = 0, int n = 0);
+
+void modify(int arr[], int n = 0, int len = 0);
+
+int* add(int arr[], int len = 0, int n = 0);
+
+void remove(int arr[], int n = 0, int len =0);
